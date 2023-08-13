3f48ce962d88b49dcb10ba532528712456b70853
paulschick <paulschick19@gmail.com>
2023-08-13 15:08:09 -0400

    Implement tailwind and postcss, add htmx
    Use postcss-cli in package.json as a build script
    Add minified htmx JavaScript and link directly in index.html
    Add hx-get request to demonstrate htmx is functioning

--- build-server.mjs
+++ build-server.mjs
@@ -1,13 +1,11 @@
 import * as esbuild from 'esbuild';
 
 /**
- * NOTE: index.html is served via server.js. There really isn't client-side code...
- * If I'm able to add the Tailwind CSS and HTMX stuff in the server configuration, then I don't need two projects.
- * The "client" portion is just the index.html, index.css, and index.ts files. The only reason for the index.ts
- * on the client-side was originally to load in the scss file for the index.html file. This was due to the compilation
- * and bundling methods used (Webpack + Babel). I may not need to do that with esbuild.
- *
- * So hang tight. This prototype might be transformed in the next iteration to a single project.
+ * Currently, the index.html file and all TSX is going to be served via server.js. In my previous project, I created
+ * an additional typescript file just for the client-side. I don't need to do that. I linked to the htmx javascript
+ * file in index.html, and I'm able to make HTTP requests directly from this file without a proxy. I'm able to do this
+ * because the HTML is being served on the server, along with all requests from HTMX. Previously I was running
+ * two servers: one for the client, and one for the server.
  */
 await esbuild.build({
     entryPoints: ['src/server/server.mjs'],
