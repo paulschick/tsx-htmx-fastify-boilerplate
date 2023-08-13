23da61175c8c86751baa1ac7895fe7c82d4e682c
paulschick <paulschick19@gmail.com>
2023-08-13 15:11:42 -0400

    clean up fastify, tsx, typescript, esbuild, tailwind boilerplate project

---
+++ build.mjs
@@ -0,0 +1,15 @@
+import * as esbuild from 'esbuild';
+
+/**
+ * The `index.html` file is served from the server. All jsx will be served from the same server, so I can get away
+ * without a client-side bundle.js (as long as no JavaScript is needed on the client, outside HTMX).
+ */
+await esbuild.build({
+    entryPoints: ['src/server.mjs'],
+    bundle: true,
+    minify: true,
+    // packages: 'external', // uncomment to reference node modules instead of bundling them
+    outfile: 'dist/server.js',
+    platform: 'node',
+    target: ['node18.16'], // remove if using packages: 'external'
+});
